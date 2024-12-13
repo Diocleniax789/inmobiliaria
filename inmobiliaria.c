b27782438675f244405a0dcf1dee400ea77987fd
Diocleniax789 <julioramdom123@gmail.com>
2024-12-13 15:31:51 -0300

    se ha completado el programa principal

--- inmobiliaria.c
+++ inmobiliaria.c
@@ -12,12 +12,23 @@ void carga_categorias(char categorias[6][10]);
 struct inmueble * valida_avisos(struct inmueble*,int,int*,char categoria[6][10]);
 int valida_tel_cat(char*,char*,char[6][10]);
 int valida_duplicidad_aviso(char*,struct inmueble*);
-void imprime(struct inmueble*);
+void imprime(struct inmueble*,int);
 
 int main(){
+    struct inmueble todos_los_avisos[1500];
+    struct inmueble *avisos_validos;
+    int cantidad_avisos_cargados = 0,cantidad_avisos_validos = 0;
+    char categorias[6][10];
 
-
-
+    carga_avisos(todos_los_inmuebles,&cantidad_avisos_cargados);
+    carga_categorias(categorias);
+    avisos_validos = valida_avisos(todos_los_avisos,cantidad_avisos_cargados,&cantidad_avisos_validos,categorias);
+    imprime(avisos_validos,cantidad_avisos_validos);
 
     return 0;
 }
+
+
+
+
+
