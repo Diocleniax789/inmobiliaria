26607e9e0b2fc0733c816f8254b3c4cb6690efdd
Diocleniax789 <julioramdom123@gmail.com>
2024-12-13 15:23:47 -0300

    se han creado los prototipos y el dato de tipo struct

--- inmobiliaria.c
+++ inmobiliaria.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+struct inmueble{
+    char cod_usuario[10];
+    char descrip_aviso[300];
+    char categoria[10];
+    char telefono[15];
+};
+
+void carga_avisos(struct inmueble*,int*);
+void carga_categorias(char categorias[6][10]);
+struct inmueble * valida_avisos(struct inmueble*,int,int*,char categoria[6][10]);
+int valida_tel_cat(char*,char*,char[6][10]);
+int valida_duplicidad_aviso(char*,struct inmueble*);
+void imprime(struct inmueble*);
+
 int main(){
 
 
